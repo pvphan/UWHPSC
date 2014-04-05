e56b82daa2b9d6db53002937173d0f02bb3b451c
pvphan <pvphan90@gmail.com>
2014-04-05 11:37:06 -0700

    script as function

--- mysqrt.py
+++ mysqrt.py
@@ -1,7 +1,13 @@
 # Coursera - UWHPSC - W1-L3.4
-x = 9.
-s = 1.
-for k in range(6):
-	print "Before iteration %s, s = %s" % (k,s)
-	s = 0.5 * (s + x/s)
-print "After %s iterations, s = %s" % (k,s)
+def sqrt2(x):
+	s = 1.
+	kmax = 100
+	tol = 1.e-14
+	for k in range(kmax):
+		print "Before iteration %s, s = %20.15f" % (k,s)
+		s0 = s
+		s = 0.5 * (s + x/s)
+		delta_s = s - s0
+		if abs(delta_s/x) < tol:
+			break
+	print "After %s iterations, s = %20.15f" % (k,s)
