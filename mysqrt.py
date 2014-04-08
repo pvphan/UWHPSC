810ca31b83a35f6b88c1f1de69f8dd0d6758ba38
pvphan <pvphan90@gmail.com>
2014-04-07 19:57:19 -0700

    sqrt2 has debug arg

--- mysqrt.py
+++ mysqrt.py
@@ -1,19 +1,25 @@
 """
-Coursera - UWHPSC - W1-L3.4
-mysqrt.py
+Module for approximating sqrt.
+More ...
 """
-def sqrt2(x):
-	"""
-	Docstring of sqrt2 function
-	"""
-	s = 1.
-	kmax = 100
-	tol = 1.e-14
-	for k in range(kmax):
-		print "Before iteration %s, s = %20.15f" % (k,s)
-		s0 = s
-		s = 0.5 * (s + x/s)
-		delta_s = s - s0
-		if abs(delta_s/x) < tol:
-			break
-	print "After %s iterations, s = %20.15f" % (k,s)
+
+def sqrt2(x, debug=False):
+    """
+    more details.
+    """
+    s = 1.
+    kmax = 100
+    tol = 1.e-14
+    for k in range(kmax):
+        if debug:
+            print "Before iteration %s, s = %20.15f" % (k,s)
+        s0 = s
+        s = 0.5 * (s + x/s)
+        delta_s = s - s0
+        if abs(delta_s / x) < tol:
+            break
+    if debug:
+        print "After %s iterations, s = %20.15f" % (k+1,s)  
+    return s
+
+  
